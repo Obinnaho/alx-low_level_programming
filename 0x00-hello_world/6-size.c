3d5e19752f812fe99a49b146e6159d1c8e3d9858
Obinna <obinnahovingi@gmail.com>
2023-07-13 21:07:28 -0700

    My first C commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+/**
+ * main -main block
+ *Return: 0
+ */
+int main(void)
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of short: %zu byte(s)\n", sizeof(int));
+printf("Size of int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long: %zu byte(s)\n", sizeof(long long int));
+printf("Size of long long: %zu byte(s)\n", sizeof(float));
+
+return (0);
+}
